c3e4fca2a62c8477ccfd9ed2ec45434be1cff44d
nv-edwli <edwli@nvidia.com>
2024-02-01 00:13:15 +0000

    Modified 3 files
    - Modified files: code/init/init-13b.sh, code/init/init-70b.sh,
    code/init/init-7b.sh

--- code/init/init-70b.sh
+++ code/init/init-70b.sh
@@ -6,4 +6,4 @@ apt-get -y install git-lfs
 cd /project/models && git lfs clone https://$HF_USERNAME:$HF_API_KEY@huggingface.co/meta-llama/Llama-2-70b-hf
 
 # Convert the model weights to NeMo format
-python /opt/NeMo/scripts/nlp_language_modeling/convert_hf_llama_to_nemo.py --in-file=../../models/Llama-2-70b-hf/ --out-file=../../models/llama-70b.nemo
+python /opt/NeMo/scripts/nlp_language_modeling/convert_hf_llama_to_nemo.py --in-file=/project/models/Llama-2-70b-hf/ --out-file=/project/models/llama-70b.nemo
