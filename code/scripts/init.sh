2dcfd1fa498c4ad90d99d84189d5b386e0462c29
nv-edwli <edwli@nvidia.com>
2024-02-02 00:26:14 +0000

    Modified code/scripts/init.sh
    - Modified files: code/scripts/init.sh

--- code/scripts/init.sh
+++ code/scripts/init.sh
@@ -1,14 +1,14 @@
 echo "Initializing the Llama-2 ${LLAMA2_SIZE} model."
 
 if [ ! -f /project/models/llama-$LLAMA2_SIZE.nemo ]; then
-    # Ensure git-lfs is available
+    echo "Ensuring git-lfs is available"
     curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
     apt-get -y install git-lfs
 
-    # Clone the model weights to this project
+    echo "Cloning the model weights"
     cd /project/models && git lfs clone https://$HF_USERNAME:$HF_API_KEY@huggingface.co/meta-llama/Llama-2-$LLAMA2_SIZE-hf
 
-    # Convert the model weights to NeMo format
+    echo "Converting the model weights to NeMo format"
     python /opt/NeMo/scripts/nlp_language_modeling/convert_hf_llama_to_nemo.py --in-file=/project/models/Llama-2-$LLAMA2_SIZE-hf/ --out-file=/project/models/llama-$LLAMA2_SIZE.nemo
     echo "llama-$LLAMA2_SIZE.nemo is ready"
 else
